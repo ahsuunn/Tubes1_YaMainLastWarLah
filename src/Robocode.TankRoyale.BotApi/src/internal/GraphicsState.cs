cccfd7e010b0bc9a19238a8e7f09ed3c8463635c
fithrarzk <13523049@std.stei.itb.ac.id>
2025-03-12 20:57:18 +0700

    feat: add bot fithra

---
+++ src/Robocode.TankRoyale.BotApi/src/internal/GraphicsState.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Robocode.TankRoyale.BotApi.Internal;
+
+public class GraphicsState
+{
+    public string GetSvgOutput()
+    {
+        return "";
+    }
+
+    public void Clear()
+    {
+    }
+}
