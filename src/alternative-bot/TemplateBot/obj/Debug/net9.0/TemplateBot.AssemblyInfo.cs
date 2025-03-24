d7a0f2f3f66757beae9a39f66708487a4c389d63
fithrarzk <13523049@std.stei.itb.ac.id>
2025-03-24 12:48:15 +0700

    feat: titip

--- src/alternative-bot/TemplateBot/obj/Debug/net9.0/TemplateBot.AssemblyInfo.cs
+++ src/alternative-bot/TemplateBot/obj/Debug/net9.0/TemplateBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdee4457cae4026adb908dfc818324eefd3f06af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3029daac7b2574130cca4ad49e0e85b91992b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
